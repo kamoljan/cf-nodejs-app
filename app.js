476f27a844c5f59213a140bc532a985eb248c139
Kamol Mavlonov <kamoljan@gmail.com>
2017-12-22 10:11:28 +0900

    Add hardcoded download script for Tam

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 
+var http = require('http');
+var fs = require('fs');
+
+
 app.get('/', function (req, res) {
+  var file = fs.createWriteStream("100MB.zip");
+  var request = http.get("http://90.130.70.73/100MB.zip", function (response) {
+    response.pipe(file);
+  });
   res.send('Hello World!')
 })
 
